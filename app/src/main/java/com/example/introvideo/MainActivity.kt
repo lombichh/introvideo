be4ae05d19e9fc1376f7f7b0631b79f5b99c7d4c
LoMbiChh <lombardi.matteo484@gmail.com>
2023-10-09 23:49:17 +0200

    add password settings activity

--- app/src/main/java/com/example/introvideo/MainActivity.kt
+++ app/src/main/java/com/example/introvideo/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.introvideo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var settingsImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        settingsImageView = findViewById(R.id.settings_imageview)
+
+        settingsImageView.setOnClickListener{
+            openSettings()
+        }
+    }
+
+    private fun openSettings() {
+        startActivity(Intent(this, PasswordSettingsActivity::class.java))
     }
 }
